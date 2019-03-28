1edcfd576d98a136298406e833fecfbb5a6bc74e
diegomendezp <diegomen15@gmail.com>
2019-03-28 11:46:56 +0100

    Styling... 💅

--- target/classes/static/js/profesional.js
+++ target/classes/static/js/profesional.js
@@ -6,7 +6,16 @@ const displayServices = () => {
     .then((data) => {
       const container = document.querySelector('#results');
       container.innerHTML = '';
-      data._embedded.servicios.forEach((service) => { container.innerHTML += `<li>${service.nombre}</li>`; });
+      data._embedded.servicios.forEach((s) => {
+        container.innerHTML += `<div class="service" serviceId='${s.id}'><img class="map" src="/images/map.png" alt="map" />
+      <div class="service-description">
+          <p class='service-category'>${s.categoria}</p>
+          <h3>${s.nombre}</h3>
+          <p class='service-price'>${s.precio_total} $</p>
+          <div class="service-buttons"><button class="see-more" id="${s.id}">More</button><button class="apply" onclick="openModal(${s.id})">Apply</button></div>
+      </div>
+    </div>`;
+      });
     });
 };
 
@@ -33,10 +42,18 @@ const addServicio = () => {
 const getServicioById = () => {
   const id = $('#idservicio').val();
   apiHandler.getOneRegister(id)
-    .then((data) => {
+    .then((s) => {
       const container = document.querySelector('#singleService');
       container.innerHTML = '';
-      container.innerHTML += `<li>${data.nombre}</li><li>${data.descripcion}</li>`;
+      s.nombre != undefined
+        ? container.innerHTML += `<div class="service" serviceId='${s.id}'><img class="map" src="/images/map.png" alt="map" />
+      <div class="service-description">
+          <p class='service-category'>${s.categoria}</p>
+          <h3>${s.nombre}</h3>
+          <p class='service-price'>${s.precio_total} $</p>
+          <div class="service-buttons"><button class="see-more" id="${s.id}">More</button><button class="apply" onclick="openModal(${s.id})">Apply</button></div>
+      </div>
+    </div>` : container.innerHTML ='<h4>NO RESULTS</h4>';
     })
     .catch(err => console.error(err));
 };
